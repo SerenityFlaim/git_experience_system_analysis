7f7a37df8655346253fad6c1a8f9b302e578b47d
SerenityFlaim <phil_th@mail.ru>
2024-11-21 11:46:13 +0300

    comments added for more detail

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,13 @@
 ﻿#include<iostream>
 
+//our main function
 int main() {
+	//prompting for user's name
 	std::string name;
 	std::cout << "Enter your name -> ";
 	std::cin >> name;
+
+	//forming output
 	std::cout << std::endl << "Hello world from " << name << "!\n";
 	return 0;
 }
